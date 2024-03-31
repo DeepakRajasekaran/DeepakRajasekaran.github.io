9eb3776cf6c6430d3bdd01c1ab301b5addf99539
“DeepakRajasekaran” <rajasekarand375@gmail.com>
2024-03-31 11:43:07 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -261,6 +261,22 @@
    * Initiate Pure Counter 
    */
   new PureCounter();
+/**
+ * Loader
+ * **/
+$.get("/assets/html/loader.html", function(data){
+  $("#loading").replaceWith(data);
+});
+$(window).on('load', function(){
+  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( ".loader" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( ".loader" ).remove(); //makes page more lightweight 
+  });   
+}
+/** the end */
 
 })()
 
